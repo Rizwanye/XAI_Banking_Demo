8c6f30a99eecadcd23ece654e969aba6a7c58454
Rizwan <kevinye18@hotmail.com>
2023-11-08 21:39:20 +1100

    Customisation of Sidebar

--- main.py
+++ main.py
@@ -21,28 +21,30 @@ st.write(""""
 The model will show you the reason why your loan was approved
 """)
 
-dataset_name = st.sidebar.selectbox("Select Dataset", ("Banking Dataset", "Breast Cancer"))
-st.write(dataset_name)
-
+# Load your dataset
 file_path = "Banking_Dataset.csv"
-# Read the CSV file into a DataFrame
 df = pd.read_csv(file_path)
 
-# Remove the "Loan_ID" column
+# Preprocess the data as you did in your code
 banking_dataset = df.drop(columns="Loan_ID")
-
-# Drop rows with missing values
 banking_dataset = banking_dataset.dropna()
-banking_dataset = df.drop(columns="Loan_ID")
-
 banking_dataset = pd.get_dummies(banking_dataset, columns=['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area'])
 banking_dataset['Loan_Status'] = banking_dataset['Loan_Status'].map({'Y': 1, 'N': 0})
 X = banking_dataset.drop(columns=['Loan_Status'])
 y = banking_dataset['Loan_Status']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# Define the list of categorical indices based on your column names
+categorical_features = [i for i, col in enumerate(X.columns) if col.startswith(('Gender_', 'Married_', 'Dependents_', 'Education_', 'Self_Employed_', 'Property_Area_'))]
+
+# Train your Decision Tree model
 clf = DecisionTreeClassifier()
-clf = clf.fit(X_train,y_train)
+clf = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+st.sidebar.slider("Income Per Year", 0, 10000)
+st.sidebar.slider("Co-Applicant-Income Per Year", 0, 10000)
+st.sidebar.slider("Loan Amount", 0, 10000)
+st.sidebar.selectbox("Loan Term Duration", (120, 180, 240, 300, 360, 480))
