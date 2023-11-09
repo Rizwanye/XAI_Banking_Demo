86501638111bac2d358ab7ea44e453ce29063158
Rizwan <kevinye18@hotmail.com>
2023-11-10 02:37:10 +1100

    Fixed button to open to the Loan app page.

--- Home.py
+++ Home.py
@@ -1,29 +1,35 @@
 import streamlit as st
-import csv
-import pandas as pd
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score, accuracy_score
-from interpret.blackbox import LimeTabular
-from interpret import show
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
 from sidebar_logo import add_logo
+from streamlit_extras.switch_page_button import switch_page
+
+### Miscallaneous Side bar stuff
 add_logo("xai_logo.jpg")
-st.markdown("# Home")
-
-print("hello world!")
-st.title("Banking APP")
-st.write(""""
-# Explore the reason why you recieve or decline loan?
-The model will show you the reason why your loan was approved
-""")
-
 st.sidebar.markdown(
     "This web app was created by Rizwan Ye as part of his Honours Project. You can find him on [GitHub](https://github.com/Rizwanye). \n\n**Supervisors From UTS:**\n- Shoujin Wang \n- Jianlong Zhou "
 )
+
+## Style of button click
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #ed6c01;
+    color:#ffffff;
+}
+div.stButton > button:hover {
+    background-color: #ffffff;
+    color:#ff0000;
+    }
+</style>""", unsafe_allow_html=True)
+
+
+### Main page description of project
+
+st.title("XAI Bank: A Holistic Way To Understand Your Loan Application")
+click = st.button("Access XAI Loan Application")
+if click:
+    switch_page("Loan Application")
+
+### Bottom information
+
+
+
