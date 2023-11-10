3e014c3531a4f7afdfe07ec2525f0426ef823cd0
Rizwan <kevinye18@hotmail.com>
2023-11-10 15:38:52 +1100

    Updated cosmetic of function.

--- pages/loan_application.py
+++ pages/loan_application.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from sidebar_logo import add_logo
 from model import predict_loan_approval, explain_prediction, clf, explained_list
+from radar_chart import plot_feature_importance
 
 favicon = "favicon.png"
 st.set_page_config(
@@ -24,7 +25,36 @@ loan_amount = st.sidebar.slider("Loan Amount", 0, 10000)
 loan_term = st.sidebar.selectbox("Loan Term Duration", (120, 180, 240, 300, 360, 480))
 area = st.sidebar.selectbox("Property Area", ("Rural", "Semiurban", "Urban"))
 
-input_values = {
+
+import streamlit as st
+import time
+
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
+st.title("Bank Loan Application Explainer")
+st.markdown(
+            "<p style='font-size: 20px;'>To use the XAI bank application, please fill the information on the side then click on the button below.</p>",
+            unsafe_allow_html=True)
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #ed6c01;
+    color:#ffffff;
+}
+
+</style>""", unsafe_allow_html=True)
+
+
+if st.button("Calculate Loan Eligibility"):
+    input_values = {
         'ApplicantIncome': income,
         'CoapplicantIncome': co_applicant_income,
         'LoanAmount': loan_amount,
@@ -32,7 +62,7 @@ input_values = {
         'Gender_Female': 1 if gender == "Female" else 0,
         'Gender_Male': 1 if gender == "Male" else 0,
         'Married_No': 1 if married == "No" else 0,
-        'Married_Yes': 1 if married == "No" == "Yes" else 0,
+        'Married_Yes': 1 if married == "Yes" else 0,
         'Dependents_0': 1 if dependents == 0 else 0,
         'Dependents_1': 1 if dependents == 1 else 0,
         'Dependents_2': 1 if dependents == 2 else 0,
@@ -40,38 +70,43 @@ input_values = {
         'Education_Graduate': 1 if graduated == "Yes" else 0,
         'Education_Not Graduate': 1 if graduated == "No" else 0,
         'Self_Employed_No': 1 if employment == "No" else 0,
-        'Self_Employed_Yes': 1 if employment== "Yes" else 0,
+        'Self_Employed_Yes': 1 if employment == "Yes" else 0,
         'Property_Area_Rural': 1 if area == "Rural" else 0,
         'Property_Area_Semiurban': 1 if area == "Semiurban" else 0,
         'Property_Area_Urban': 1 if area == "Urban" else 0
-}
+    }
+    st.title("Radar Chart Output")
+    st.markdown(
+            "<p style='font-size: 20px;'>This visualization shows the most important features used to determine the Loan application outcome.</p>",
+            unsafe_allow_html=True)
 
-st.title("Bank Loan Application Explainer")
-st.markdown("<p style='font-size: 20px;'>This visualization shows the most important features used to determine the Loan application outcome.</p>", unsafe_allow_html=True)
+    prediction = predict_loan_approval(input_values, clf)
 
+    prediction_label = 'Your application has been <b>APPROVED</b>, below the orange coloured bars have helped you receive this loan.' if \
+    prediction[
+            0] == 1 else 'We apologize, your application has been <b>DECLINED</b>, you might want to look at the features highlighted in dark purple.'
 
-prediction = predict_loan_approval(input_values, clf)
-
-prediction_label = 'Your application has been <b>APPROVED</b>, below the orange coloured bars have helped you receive this loan.' if prediction[0] == 1 else 'We apologize, your application has been <b>DECLINED</b>, you might want to look at the features highlighted in dark purple.'
-
-# Using st.write with Markdown for font size and HTML for bold text
-st.write(f"<p style='font-size: 20px; padding-bottom: 20px;'>{prediction_label}</p>", unsafe_allow_html=True)
-
-def explanation_list_output():
-        prediction = predict_loan_approval(input_values, clf)
-        if prediction[0] == 0:
-                inverted_explanation_list = explained_list(input_values, clf)
-                update = [(key, value) for key, value in inverted_explanation_list if
-                                 not (key.startswith("Dependents_") and input_values.get(key, 0) == 0)]
-                explanation_list= [(feature, -value) for feature, value in update]
-        else:
-                explanation_list_1 = explained_list(input_values, clf)
-                explanation_list = [(key, value) for key, value in explanation_list_1 if
-                  not (key.startswith("Dependents_") and input_values.get(key, 0) == 0)]
-
-        return explanation_list
+    # Using st.write with Markdown for font size and HTML for bold text
+    st.write(f"<p style='font-size: 20px; padding-bottom: 20px;'>{prediction_label}</p>", unsafe_allow_html=True)
 
 
-from radar_chart import plot_feature_importance
-explanation_list = explanation_list_output()
-plot_feature_importance(explanation_list)
+    def explanation_list_output():
+            prediction = predict_loan_approval(input_values, clf)
+            if prediction[0] == 0:
+                    inverted_explanation_list = explained_list(input_values, clf)
+                    update = [(key, value) for key, value in inverted_explanation_list if
+                              not (key.startswith("Dependents_") and input_values.get(key, 0) == 0)]
+                    explanation_list = [(feature, -value) for feature, value in update]
+            else:
+                    explanation_list_1 = explained_list(input_values, clf)
+                    explanation_list = [(key, value) for key, value in explanation_list_1 if
+                                        not (key.startswith("Dependents_") and input_values.get(key, 0) == 0)]
+
+            return explanation_list
+
+
+    explanation_list = explanation_list_output()
+    plot_feature_importance(explanation_list)
+
+
+
